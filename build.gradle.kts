1ffa88b714725a28999af262eb85c2564ce47f05
BBaoVanC <bbaovanc@bbaovanc.com>
2023-03-25 19:05:39 -0500

    Update dependencies & remove junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,5 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
 }
